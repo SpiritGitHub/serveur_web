35d760ceac653c1df93ce9b477f8516ceabbb79f
Azhar <103309031+SpiritGitHub@users.noreply.github.com>
2023-12-02 11:20:04 +0000

    fait

--- public/code.js
+++ public/code.js
@@ -1,49 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
+(function() {
     const app = document.querySelector("#app");
-    const socket = io(); // Assurez-vous que le serveur Socket.io est correctement configuré et accessible
+    const socket = io();
 
-    let uname; // Variable pour stocker le nom d'utilisateur actuel
+    let uname;
 
-    // Gestion de l'événement 'click' pour rejoindre le chat
-    // Gestion de l'événement 'click' pour rejoindre le chat
-    app.querySelector("#join-user").addEventListener("click", function(){
-        let username = app.querySelector("#username").value;
+    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+        let username = app.querySelector(".join-screen #username").value;
         if (username.length === 0) {
-            alert("Veuillez entrer un nom d'utilisateur.");
             return;
         }
+        socket.emit("newuser", username);
         uname = username;
-        socket.emit("newuser", username); // Envoyer le nouveau nom d'utilisateur au serveur
         app.querySelector(".join-screen").classList.remove("active");
-        app.querySelector(".chat-group").classList.add("active"); // Modifier ici pour cibler .chat-group
+        app.querySelector(".chat-screen").classList.add("active");
         app.querySelector(".connected-users").classList.add("active");
     });
 
-
-    // Gestion de l'événement 'click' pour envoyer un message
-    app.querySelector("#send-message").addEventListener("click", function(){
-        let message = app.querySelector("#message-input").value; 
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+        console.log("Button clicked!");
+        let message = app.querySelector(".chat-screen #message-input").value; 
+        console.log("Message:", message);
+        
         if (message.length === 0) {
             return;
         }
         renderMessage("my", {
             username: uname,
-            text: message
+            text: message,
         });
-        socket.emit("chat", { // Envoyer le message au serveur pour diffusion
+        socket.emit("chat", {
             username: uname,
-            text: message
+            text: message,
         });
-        app.querySelector("#message-input").value = ""; // Vider le champ de saisie
+        app.querySelector(".chat-screen #message-input").value = "";
     });
 
-    // Gestion de l'événement 'click' pour quitter le chat
-    app.querySelector("#exit-chat").addEventListener("click",function(){
-        socket.emit("exituser", uname); // Avertir le serveur que l'utilisateur quitte
-        window.location.reload(); // Recharger la page ou rediriger l'utilisateur comme vous préférez
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser", uname);
+        window.location.href = window.location.href;
     });
 
-    // Réception des mises à jour du serveur
     socket.on("update", function(update){
         renderMessage("update", update);
     });
@@ -52,36 +48,34 @@ document.addEventListener('DOMContentLoaded', function () {
         renderMessage("other", message);
     });
 
-    // Fonction pour afficher les messages sur l'écran
     function renderMessage(type, message){
-        let messageContainer = app.querySelector(".messages");
-        let el = document.createElement("div");
-        el.classList.add("message", type === "my" ? "my-message" : "other-message");
-        el.innerHTML = `
-            <div class="username">${type === "my" ? 'You' : message.username}</div>
-            <div class="text">${message.text}</div>
-        `;
-        messageContainer.appendChild(el);
-        messageContainer.scrollTop = messageContainer.scrollHeight; // Faire défiler vers le nouveau message
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if(type === "my"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "message my-message");
+            el.innerHTML = `
+                <div>
+                    <div class="name">you</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        }else if(type === "other"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "message other-message");
+            el.innerHTML = `
+                <div>
+                    <div class="name">${message.username}</div>
+                    <div class="text">${message.text}</div>
+                </div>
+            `;
+            messageContainer.appendChild(el);
+        }else if(type === "update"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
-
-    // Fonction pour mettre à jour la liste des utilisateurs connectés
-    socket.on("updateUsersList", function(users){
-        const userList = document.getElementById('user-list');
-        userList.innerHTML = ''; // Vider la liste existante
-
-        users.forEach(user => {
-            const listItem = document.createElement('li');
-            listItem.textContent = user;
-            listItem.addEventListener('click', () => startPrivateChat(user));
-            userList.appendChild(listItem);
-        });
-    });
-
-    // Fonction pour démarrer un chat privé
-    function startPrivateChat(username) {
-        console.log('Démarrer une discussion privée avec', username);
-        // Ici, vous pourriez changer d'interface pour le chat privé ou gérer autrement
-        // Assurez-vous que le serveur sait qu'il s'agit d'un message privé
-    }
-});
+})();
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-// Utiliser pour garder une trace des utilisateurs connectés
 let users = {};
 
 app.use(express.static(path.join(__dirname + "/public")));
@@ -12,47 +11,34 @@ app.use(express.static(path.join(__dirname + "/public")));
 io.on("connection", function(socket){
 
     socket.on("newuser", function(username){
-        // Ajouter l'utilisateur à l'objet 'users'
         users[socket.id] = username;
-        // Informer le nouvel utilisateur de tous les autres utilisateurs connectés
         socket.emit("updateUsersList", Object.values(users));
-        // Informer tous les utilisateurs de la connexion d'un nouvel utilisateur
         socket.broadcast.emit("update", username + " a rejoint la conversation");
-        // Mettre à jour la liste des utilisateurs pour tout le monde
         io.emit("updateUsersList", Object.values(users));
     });
 
     socket.on("exituser", function(username){
-        // Supprimer l'utilisateur de l'objet 'users'
         delete users[socket.id];
-        // Informer tous les utilisateurs de la déconnexion de l'utilisateur
         socket.broadcast.emit("update", username + " a quitté la conversation");
-        // Mettre à jour la liste des utilisateurs pour tout le monde
         io.emit("updateUsersList", Object.values(users));
     });
 
     socket.on("chat", function(message){
-        // Diffuser le message à tous les autres utilisateurs
         socket.broadcast.emit("chat", message);
     });
 
-    // Gérer les messages privés
     socket.on("private_chat", function(data){
-        // Envoi d'un message privé à un utilisateur spécifique
+       
         socket.to(data.to).emit("private_chat", {
             from: socket.id,
             message: data.message
         });
     });
 
-    // Gérer la déconnexion de l'utilisateur
     socket.on("disconnect", function(){
         if(users[socket.id]){
-            // Informer tous les utilisateurs de la déconnexion
             socket.broadcast.emit("update", users[socket.id] + " a quitté la conversation");
-            // Supprimer l'utilisateur de l'objet 'users'
             delete users[socket.id];
-            // Mettre à jour la liste des utilisateurs pour tout le monde
             io.emit("updateUsersList", Object.values(users));
         }
     });
